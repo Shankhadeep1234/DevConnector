ae9f7a6a4cfec164d51a0aa11e58b22a47efbf91
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-18 14:54:22 +0530

    inital commit and initial server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API running"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log(`Server stared on port: ${port}`);
+});
