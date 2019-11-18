55950b06f9d587bb46456b97a9fb98c573972f4d
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-18 15:11:45 +0530

    connected to DB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect to database
+connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
