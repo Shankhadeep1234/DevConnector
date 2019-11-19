329be9d131e84971d46272d46626fa7ee2f8cbbf
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-19 14:00:13 +0530

    done with the users route

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 //connect to database
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
